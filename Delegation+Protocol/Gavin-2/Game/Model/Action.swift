fff7f089e3363f247430a57deabc9d79d50b557a
PinguMac <PinguMac@Mac.local>
2018-02-11 23:24:32 +0800

    Modify action name

--- Delegation+Protocol/Gavin-2/Game/Model/Action.swift
+++ Delegation+Protocol/Gavin-2/Game/Model/Action.swift
@@ -9,26 +9,28 @@
 import Foundation
 
 enum ActionResult {
-  case Tie, ActorWin, ActorLose
+  case Tie
+  case Win
+  case Lose
 }
 
 protocol Action {
   var name: String { get }
 }
 
-class Paper: Action {
+class Attack: Action {
   var name = "attack"
 }
 
-class Scissors: Action {
+class Defence: Action {
   var name = "defence"
 }
 
-class Stone: Action {
+class CounterAttack: Action {
   var name = "counterattack"
 }
 
 protocol ActionDelegate: class {
-  func actionDidSelect(action: Action)
+  func actionDidSelect(actorAction: Action)
 }
 
