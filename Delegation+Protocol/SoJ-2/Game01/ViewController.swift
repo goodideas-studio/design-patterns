d4f0f8e27c30f5a01cae9c5ed96d299f90046d8f
SOJ <takayashi0825@gmail.com>
2018-02-12 11:34:07 +0800

    Update Delegation+Protocol/SoJ-2
    newGameRule
    Add notification sample
    Add attack animation
    Add HP MP layer

--- Delegation+Protocol/SoJ-2/Game01/ViewController.swift
+++ Delegation+Protocol/SoJ-2/Game01/ViewController.swift
@@ -12,46 +12,47 @@ class ViewController: UIViewController, showDataDelegate {
 
     @IBOutlet var playerImageView: UIImageView!
     @IBOutlet var playerHPLabel: HpLabel!
+    @IBOutlet var playerMPLabel: HpLabel!
     @IBOutlet var monsterImageView: UIImageView!
     @IBOutlet var monsterHPLabel: HpLabel!
+    @IBOutlet var monsterMPLabel: HpLabel!
     @IBOutlet var gameLabel: UILabel!
+    @IBOutlet var playerAttackAnimateImageView: UIImageView!
+    @IBOutlet var ｍonsterAttackAnimateImageView: UIImageView!
     
     let gameBoard = GameBoard()
-    var gameRound: String?
+    var gameRound: [String] = []
     
     
     @IBAction func attack(_ sender: Any) {
-        //showAction(action: "攻擊")
-        //playerImageView.image = UIImage(named:"攻擊")
-//        monsterAction = monsterSkill[Int(arc4random_uniform(UInt32(monsterSkill.count)))]
         gameBoard.fight(playerAction: "ATK")
-        
     }
-    @IBAction func defense(_ sender: Any) {
-        //showAction(action: "防禦")
-        //playerImageView.image = UIImage(named:"防禦")
-        gameBoard.fight(playerAction: "DEF")
+    @IBAction func magicAttack(_ sender: Any) {
+        gameBoard.fight(playerAction: "MATK")
     }
-    @IBAction func squelch(_ sender: Any) {
-        //showAction(action: "反擊")
-        //playerImageView.image = UIImage(named:"反擊")
-        gameBoard.fight(playerAction: "SQU")
-        
+    @IBAction func heal(_ sender: Any) {
+        gameBoard.fight(playerAction: "HEAL")
     }
     
     @IBAction func switchCharacter(_ sender: Any) {
-        //playerHPLabel.text = "1000"
-       // playerImageView.image = UIImage(named:"招喚師攻擊")
         gameBoard.switchCharacter()
-//        showHP(playerHP: <#T##Int#>, monsterHP: <#T##Int#>)
-        
+    }
+    @IBAction func reStartGame(_ sender: Any) {
+        gameBoard.setCharacter()
     }
     func showAction(action: String) {
         playerImageView.image = UIImage(named: action)
     }
-    func showHP(playerHP: Int, monsterHP: Int) {
+    func showHP(playerHP: Int, monsterHP: Int, playerHPRatio: Double, monsterHPRatio: Double) {
         playerHPLabel.text = "HP: \(String(playerHP))"
         monsterHPLabel.text = "HP: \(String(monsterHP))"
+        createPlayerHPLayer(ratio: playerHPRatio)
+        createMonsterHPLayer(ratio: monsterHPRatio)
+    }
+    func showMP(playerMP: Int, monsterMP: Int, playerMPRatio: Double, monsterMPRatio: Double) {
+        playerMPLabel.text = "MP: \(String(playerMP))"
+        monsterMPLabel.text = "MP: \(String(monsterMP))"
+        
     }
     
     func showLose(lose: String) {
@@ -61,19 +62,189 @@ class ViewController: UIViewController, showDataDelegate {
     func showWin(win: String) {
         gameLabel.text = win
     }
+    
+// Notification
+    @objc func playerAttackAnimate(notification: NSNotification) {
+        //物理攻擊動畫
+//        let imageView = UIImageView(frame: CGRect(x: 277 + monsterImageView.center.x , y: 29 + monsterImageView.center.y, width: 240, height: 128))
+//        imageView.image = UIImage(named: "物理攻擊")
+//        view.addSubview(imageView)
+        playerAttackAnimateImageView.bounds.size = CGSize(width: 139, height: 210)
+        //125 189
+        playerAttackAnimateImageView.center = monsterImageView.center
+            //CGRect(x: 277 + monsterImageView.center.x , y: 29 + monsterImageView.center.y, width: 112, height: 169)
+        playerAttackAnimateImageView.image = UIImage(named: "物理攻擊")
+        
+        // 怪物被攻擊位移
+        UIView.animate(withDuration: 0.2, animations: {
+            self.monsterImageView.center.x += 10
+        }) { (finish) in
+            UIView.animate(withDuration: 0.2) {
+                self.monsterImageView.center.x -= 10
+                DispatchQueue.main.asyncAfter(deadline: .now() + 0.2) {
+                    self.playerAttackAnimateImageView.image = UIImage(named: "")
+                }
+            }
+        }
+        
+        
+    }
+    
+    @objc func monsterAttackAnimate(notification: NSNotification) {
+        // 怪物被攻擊位移
+        UIView.animate(withDuration: 0.2, animations: {
+            self.playerImageView.center.x -= 10
+        }) { (finish) in
+            UIView.animate(withDuration: 0.2) {
+                self.playerImageView.center.x += 10
+            }
+        }
+    }
+// Delegate & protocol
+//    func playerAttackAnimate(atkType: String) {
+//        // 怪物被攻擊位移
+//        UIView.animate(withDuration: 0.2, animations: {
+//            print(self.monsterImageView.center)
+//            self.monsterImageView.center.x += 10
+//        }) { (finish) in
+//            UIView.animate(withDuration: 0.2) {
+//                self.monsterImageView.center.x -= 10
+//            }
+//        }
+//    }
+//    func monsterAttackAnimate(atkType: String) {
+//        // 玩家被攻擊位移
+//        UIView.animate(withDuration: 0.2, animations: {
+//            print(self.playerImageView.center)
+//            self.playerImageView.center.x -= 10
+//        }) { (finish) in
+//            UIView.animate(withDuration: 0.2) {
+//                self.playerImageView.center.x += 10
+//            }
+//        }
+//    }
+    
+    var playerHPLayer: CAGradientLayer!
+    var playerMPLayer: CAGradientLayer!
+    var monsterHPLayer: CAGradientLayer!
+    var monsterMPLayer: CAGradientLayer!
+    
+    func createPlayerHPLayer(ratio: Double) {
+        if let playerHPLayer = playerHPLayer {
+            playerHPLayer.removeFromSuperlayer()
+        }
+        playerHPLayer = CAGradientLayer()
+        let zeroHPColor = UIColor(red: 255/255, green: 0/255, blue: 0/255, alpha: 1).cgColor
+        let lowHPColor = UIColor(red: 255/255, green: 100/255, blue: 50/255, alpha: 1).cgColor
+        let highHPColor = UIColor(red: 213/255, green: 189/255, blue: 121/255, alpha: 1).cgColor
+        
+        var secondColorLocation = 0.2 / ratio
+        let thirdColorLocation = 1 * ratio
+        let fourthColorLocation = 1 * ratio
+        if secondColorLocation > thirdColorLocation {
+            secondColorLocation = thirdColorLocation
+        }
 
-//    let playerCharacter = PlayerAbility()
-//    let monsterCharacter = MonsterAbility()
+        playerHPLayer.frame = CGRect(x: 20, y: 121, width: 100, height: 5)
+        playerHPLayer.startPoint = CGPoint(x:0.0, y:0.5)
+        playerHPLayer.endPoint = CGPoint(x:1.0, y:0.5)
+        playerHPLayer.locations = [0, NSNumber(value: secondColorLocation), NSNumber(value: thirdColorLocation), NSNumber(value: fourthColorLocation)]
+        playerHPLayer.colors = [zeroHPColor, lowHPColor, highHPColor, UIColor.clear.cgColor]
+
+        self.view.layer.addSublayer(playerHPLayer)
+        
+    }
+    
+    func createPlayerMPLayer(ratio: Double) {
+        if let playerMPLayer = playerMPLayer {
+            playerMPLayer.removeFromSuperlayer()
+        }
+        playerMPLayer = CAGradientLayer()
+        
+        var secondColorLocation = 0.2 * ratio
+        let thirdColorLocation = 1 * ratio
+        let fourthColorLocation = 1 * ratio
+        if secondColorLocation > thirdColorLocation {
+            secondColorLocation = thirdColorLocation
+        }
+        
+        let zeroMPColor = UIColor(red: 60/255, green: 87/255, blue: 155/255, alpha: 1).cgColor
+        let lowMPColor = UIColor(red: 85/255, green: 136/255, blue: 187/255, alpha: 1).cgColor
+        let highMPColor = UIColor(red: 102/255, green: 153/255, blue: 221/255, alpha: 1).cgColor
+        
+        playerMPLayer.frame = CGRect(x: 20, y: 127, width: 100, height: 5)
+        playerMPLayer.startPoint = CGPoint(x:0.0, y:0.5)
+        playerMPLayer.endPoint = CGPoint(x:1.0, y:0.5)
+        playerMPLayer.locations = [0, NSNumber(value: secondColorLocation), NSNumber(value: thirdColorLocation), NSNumber(value: fourthColorLocation)]
+        playerMPLayer.colors = [zeroMPColor, lowMPColor, highMPColor, UIColor.clear.cgColor]
+        
+        self.view.layer.addSublayer(playerMPLayer)
+    }
+    
+    func createMonsterHPLayer(ratio: Double) {
+
+        if let monsterHPLayer = monsterHPLayer {
+        monsterHPLayer.removeFromSuperlayer()
+        }
+        monsterHPLayer = CAGradientLayer()
+        let zeroHPColor = UIColor(red: 255/255, green: 0/255, blue: 0/255, alpha: 1).cgColor
+        let lowHPColor = UIColor(red: 255/255, green: 100/255, blue: 50/255, alpha: 1).cgColor
+        let highHPColor = UIColor(red: 213/255, green: 189/255, blue: 121/255, alpha: 1).cgColor
+        
+        var secondColorLocation = 0.2 / ratio
+        let thirdColorLocation = 1.0 * ratio
+        let fourthColorLocation = 1.0 * ratio
+        if secondColorLocation > thirdColorLocation {
+            secondColorLocation = thirdColorLocation
+        }
+        
+        monsterHPLayer.frame = CGRect(x: 434, y: 41, width: 100, height: 5)
+        monsterHPLayer.startPoint = CGPoint(x:0.0, y:0.5)
+        monsterHPLayer.endPoint = CGPoint(x:1.0, y:0.5)
+        monsterHPLayer.locations = [0, NSNumber(value: secondColorLocation), NSNumber(value: thirdColorLocation), NSNumber(value: fourthColorLocation)]
+        monsterHPLayer.colors = [zeroHPColor, lowHPColor, highHPColor, UIColor.clear.cgColor]
+        
+        self.view.layer.addSublayer(monsterHPLayer)
+        
+    }
+    
+    func createMonsterMPLayer(ratio: Double) {
+        monsterMPLayer = CAGradientLayer()
+        if let monsterMPLayer = monsterMPLayer {
+            monsterMPLayer.removeFromSuperlayer()
+        }
+        let zeroMPColor = UIColor(red: 60/255, green: 87/255, blue: 155/255, alpha: 1).cgColor
+        let lowMPColor = UIColor(red: 85/255, green: 136/255, blue: 187/255, alpha: 1).cgColor
+        let highMPColor = UIColor(red: 102/255, green: 153/255, blue: 221/255, alpha: 1).cgColor
+        
+        var secondColorLocation = 0.2 / ratio
+        let thirdColorLocation = 1.0 * ratio
+        let fourthColorLocation = 1.0 * ratio
+        if secondColorLocation > thirdColorLocation {
+            secondColorLocation = thirdColorLocation
+        }
+        
+        monsterMPLayer.frame = CGRect(x: 434, y: 47, width: 100, height: 5)
+        monsterMPLayer.startPoint = CGPoint(x:0.0, y:0.5)
+        monsterMPLayer.endPoint = CGPoint(x:1.0, y:0.5)
+        monsterMPLayer.locations = [0, NSNumber(value: secondColorLocation), NSNumber(value: thirdColorLocation), NSNumber(value: fourthColorLocation)]
+        monsterMPLayer.colors = [zeroMPColor, lowMPColor, highMPColor, UIColor.clear.cgColor]
+        
+        self.view.layer.addSublayer(monsterMPLayer)
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
         gameBoard.delegate = self
-        showHP(playerHP: 2000, monsterHP: 2000)
-//        playerHPLabel.text = "HP: \(playerCharacter.hp)"
-//        playerImageView.image = UIImage(named: playerCharacter.imageName)
-//        monsterHPLabel.text = "HP: \(monsterCharacter.hp)"
-//        monsterImageView.image = UIImage(named: monsterCharacter.imageName)
+        gameBoard.setCharacter()
+        
+        createPlayerHPLayer(ratio: 1)
+        createPlayerMPLayer(ratio: 1)
+        createMonsterHPLayer(ratio: 1)
+        createMonsterMPLayer(ratio: 1)
+        NotificationCenter.default.addObserver(self, selector: #selector(playerAttackAnimate(notification:)), name: .playerAttackAnimate, object: nil)
+        NotificationCenter.default.addObserver(self, selector: #selector(monsterAttackAnimate(notification:)), name: .monsterAttackAnimate, object: nil)
     }
 
     override func didReceiveMemoryWarning() {
@@ -81,6 +252,10 @@ class ViewController: UIViewController, showDataDelegate {
         // Dispose of any resources that can be recreated.
     }
 
+}
 
+extension Notification.Name {
+    static let playerAttackAnimate = Notification.Name("plaeryAttackAnimate")
+    static let monsterAttackAnimate = Notification.Name("monsterAttackAnimate")
 }
 
--- Delegation+Protocol/SoJ-2/Game01/character/Character.swift
+++ Delegation+Protocol/SoJ-2/Game01/character/Character.swift
@@ -8,31 +8,104 @@
 
 import Foundation
 
-
-class PlayerAbility {
+class CharacterAbility {
     
-    var hp: Int = 2000
-    var atk: Int = 200
-    var imageName: [String] = []
+    var level: Int  // flee +1, DEF +1 / 2, AD & AP +10 / 4, MDF +1 / 4
+    var job: String
+    var exp: Int
+    var hp: Int
+    var mp: Int
+    var str: Int    //strength 力量 : AD +10, DEF +1
+    var vit: Int    //vitality 體質 : max HP +1%, DEF & MDEF +2
+    var int: Int    //intelligence 智力 : AP +15, MDEF +1
+    var agi: Int    //agility 敏捷 : flee +1
+    var skill: [String]     //技能
+    var imageName: String   //攻擊圖片
     
-    init(hp:Int,atk:Int, imageName: [String]) {
+    init(level: Int,job: String,exp: Int, hp: Int, mp: Int, str: Int, vit: Int, int: Int, agi: Int,skill: [String], imageName: String) {
+        
+        self.level = level
+        self.job = job
+        self.exp = exp
         self.hp = hp
-        self.atk = atk
+        self.mp = mp
+        self.str = str
+        self.vit = vit
+        self.int = int
+        self.agi = agi
+        self.skill = skill
         self.imageName = imageName
     }
     
 }
 
-class MonsterAbility {
+class CharacterAttribute {
+  
+    var levelUpExp: Int
+    var maxHp: Int
+    var maxMp: Int
+    var ATK: Int
+    var MATK: Int
+    var DEF: Int
+    var MDEF: Int
+    var FLEE: Int
     
-    var hp: Int = 2000
-    var atk: Int = 200
-    var imageName: String = "boss1"
-    
-    init(hp:Int,atk:Int, imageName:String) {
-        self.hp = hp
-        self.atk = atk
-        self.imageName = imageName
+    init(levelUpExp: Int, maxHp: Int, maxMp: Int, ATK: Int, MATK: Int, DEF: Int, MDEF: Int, FLEE: Int) {
+        
+        self.levelUpExp = levelUpExp
+        self.maxHp = maxHp
+        self.maxMp = maxMp
+        self.ATK = ATK
+        self.MATK = MATK
+        self.DEF = DEF
+        self.MDEF = MDEF
+        self.FLEE = FLEE
     }
-    
+//    var characterAbility: CharacterAbility? {
+//        didSet {
+//            if let character = characterAbility {
+//                //升級經驗值
+//                var levelUpExp: Int = character.level * 100
+//                //物理傷害
+//                var ATK: Int = character.str * 10 + Int(round(Double(character.level/4))) * 10
+//                //魔法傷害
+//                var MATK: Int = character.int * 15 + character.level * 10
+//                //物理防禦
+//                var DEF: Int = character.vit * 2 + character.str + character.level * 2
+//                //魔法防禦
+//                var MDEF: Int = character.vit * 2 + character.int + character.level / 4
+//                //閃避
+//                var FLEE: Int = character.agi + character.level
+//            }
+//
+//        }
 }
+
+//class PlayerAbility {
+//    
+//    var hp: Int = 2000
+//    var atk: Int = 200
+//    var imageName: [String] = []
+//    
+//    init(hp:Int,atk:Int, imageName: [String]) {
+//        self.hp = hp
+//        self.atk = atk
+//        self.imageName = imageName
+//    }
+//    
+//}
+//
+//class MonsterAbility {
+//    
+//    var hp: Int = 2000
+//    var atk: Int = 200
+//    var imageName: String = "boss1"
+//    
+//    init(hp:Int,atk:Int, imageName:String) {
+//        self.hp = hp
+//        self.atk = atk
+//        self.imageName = imageName
+//    }
+//    
+//}
+
