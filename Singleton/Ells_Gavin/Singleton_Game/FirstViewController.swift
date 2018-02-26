d82fbe597576e8c6df3f9b6b1f6e8767ec7ca0dd
唐嘉伶 <tangjialing@tangjialingde-MacBook-Pro.local>
2018-02-26 14:19:40 +0800

    Initial singleton design pattern project

---
+++ Singleton/Ells_Gavin/Singleton_Game/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  Singleton_Game
+//
+//  Created by 唐嘉伶 on 26/02/2018.
+//  Copyright © 2018 唐嘉伶. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
