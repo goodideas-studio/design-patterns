0f8242e5eb48c8577b0061e53bbf36d738cbfef6
shavenking <shavenking@gmail.com>
2018-02-26 14:58:12 +0800

    Add Singleton/charizard

---
+++ Singleton/charizard/charizard/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  charizard
+//
+//  Created by Andy Tsai on 26/02/2018.
+//  Copyright © 2018 Andy Tsai. All rights reserved.
+//
+
+import UIKit
+
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+    override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+    }
+
+
+
+}
