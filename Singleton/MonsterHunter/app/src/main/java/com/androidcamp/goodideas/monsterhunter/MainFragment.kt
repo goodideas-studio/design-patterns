61be6b7d5fbfb65abcc6241de99de53261994840
York <kmgs4524@gmail.com>
2018-02-26 19:43:14 +0800

    Adjust layout of fragment_main.xml

--- Singleton/MonsterHunter/app/src/main/java/com/androidcamp/goodideas/monsterhunter/MainFragment.kt
+++ Singleton/MonsterHunter/app/src/main/java/com/androidcamp/goodideas/monsterhunter/MainFragment.kt
@@ -11,18 +11,9 @@ import android.view.ViewGroup
 
 class MainFragment : Fragment() {
 
-    // TODO: Rename and change types of parameters
-    private var mParam1: String? = null
-    private var mParam2: String? = null
-
-    private var mListener: OnFragmentInteractionListener? = null
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        if (arguments != null) {
-            mParam1 = arguments.getString(ARG_PARAM1)
-            mParam2 = arguments.getString(ARG_PARAM2)
-        }
+
     }
 
     override fun onCreateView(inflater: LayoutInflater?, container: ViewGroup?,
@@ -31,46 +22,16 @@ class MainFragment : Fragment() {
         return inflater!!.inflate(R.layout.fragment_main, container, false)
     }
 
-    // TODO: Rename method, update argument and hook method into UI event
-    fun onButtonPressed(uri: Uri) {
-        if (mListener != null) {
-            mListener!!.onFragmentInteraction(uri)
-        }
-    }
-
-    override fun onAttach(context: Context?) {
-        super.onAttach(context)
-        if (context is OnFragmentInteractionListener) {
-            mListener = context
-        } else {
-            throw RuntimeException(context!!.toString() + " must implement OnFragmentInteractionListener")
-        }
-    }
-
-    override fun onDetach() {
-        super.onDetach()
-        mListener = null
-    }
-
-    interface OnFragmentInteractionListener {
-        // TODO: Update argument type and name
-        fun onFragmentInteraction(uri: Uri)
-    }
-
     companion object {
-        // TODO: Rename parameter arguments, choose names that match
-        // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-        private val ARG_PARAM1 = "param1"
-        private val ARG_PARAM2 = "param2"
 
-        // TODO: Rename and change types and number of parameters
-        fun newInstance(param1: String, param2: String): MainFragment {
-            val fragment = MainFragment()
-            val args = Bundle()
-            args.putString(ARG_PARAM1, param1)
-            args.putString(ARG_PARAM2, param2)
-            fragment.arguments = args
-            return fragment
-        }
+//        // TODO: Rename and change types and number of parameters
+//        fun newInstance(param1: String, param2: String): MainFragment {
+//            val fragment = MainFragment()
+//            val args = Bundle()
+//            args.putString(ARG_PARAM1, param1)
+//            args.putString(ARG_PARAM2, param2)
+//            fragment.arguments = args
+//            return fragment
+//        }
     }
 }// Required empty public constructor
