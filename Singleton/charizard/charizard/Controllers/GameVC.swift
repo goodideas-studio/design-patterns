12bdae6fb19ce65a4170d5cd6e7ec68a13a0c89b
ethan510010 <ethan510010@hotmail.com>
2018-02-26 15:28:39 +0800

    with Basic ViewController

---
+++ Singleton/charizard/charizard/Controllers/GameVC.swift
@@ -0,0 +1,35 @@
+//
+//  GameVC.swift
+//  charizard
+//
+//  Created by EthanLin on 2018/2/26.
+//  Copyright © 2018年 EthanLin. All rights reserved.
+//
+
+import UIKit
+
+class GameVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
