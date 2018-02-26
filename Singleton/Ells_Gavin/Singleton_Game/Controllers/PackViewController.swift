e448a825535164c0179575a456588a80352f1cad
PinguMac <>
2018-02-26 18:47:44 +0800

    Add money button notification

--- Singleton/Ells_Gavin/Singleton_Game/Controllers/PackViewController.swift
+++ Singleton/Ells_Gavin/Singleton_Game/Controllers/PackViewController.swift
@@ -9,6 +9,7 @@
 import UIKit
 
 class PackViewController: UIViewController {
+<<<<<<< Updated upstream
     
     var itemsInMyPack = [String]()
     
@@ -23,19 +24,54 @@ class PackViewController: UIViewController {
         // Do any additional setup after loading the view, typically from a nib.
     }
 =======
+=======
+  
+  @IBOutlet weak var totalAsset: UILabel!
+  @IBOutlet weak var packItemsCollection: UICollectionView!
+  
+  var itemsInMyPack = [String]()
+  var receiveData: String!
+  
+>>>>>>> Stashed changes
   override func viewDidLoad() {
     super.viewDidLoad()
     // Do any additional setup after loading the view, typically from a nib.
     
+    NotificationCenter.default.addObserver(self,
+                                           selector: #selector(moneyNotify),
+                                           name: .moneyDidSelect,
+                                           object: nil)
   }
->>>>>>> Stashed changes
 
   override func didReceiveMemoryWarning() {
     super.didReceiveMemoryWarning()
     // Dispose of any resources that can be recreated.
   }
+  
+  @IBAction func moneyNotify(notification: NSNotification) {
+    receiveData = notification.object as! String
+    totalAsset.text = String(receiveData)
+    print(notification.object!)
+  }
+  
+}
 
+extension PackViewController: UICollectionViewDelegate {
+  
+}
 
+extension PackViewController: UICollectionViewDataSource {
+  
+  func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+    
+    return itemsInMyPack.count
+  }
+  
+  func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+    let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "CellOne", for: indexPath) as! CellTypeOne
+    
+    return cell
+  }
 }
 
 extension PackViewController: UICollectionViewDelegate {
