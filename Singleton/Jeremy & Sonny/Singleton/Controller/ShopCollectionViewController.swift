de10f58b2d3f2e6f7005f1871ffb3a5c41b4f2ea
Jeremy <gg831006@gmail.com>
2018-02-26 18:44:47 +0800

    修改protocol

--- Singleton/Jeremy & Sonny/Singleton/Controller/ShopCollectionViewController.swift
+++ Singleton/Jeremy & Sonny/Singleton/Controller/ShopCollectionViewController.swift
@@ -11,13 +11,15 @@ import UIKit
 private let reuseIdentifier = "Cell"
 let shop = Shop()
 
+protocol ShopItemDelegate {
+    func getItem(itemName:String)
+}
 
 class ShopCollectionViewController: UICollectionViewController {
-    
-
-    
 
     let fullsize = UIScreen.main.bounds.size
+    var itemDelegate:ShopItemDelegate?
+    
     
     
     @IBOutlet var myCollectionView: UICollectionView!
@@ -84,8 +86,8 @@ class ShopCollectionViewController: UICollectionViewController {
             shop.shopItem.remove(at: sender.tag)
             self.myCollectionView.reloadData()
             
-            let notificationName = Notification.Name("BuyItem")
-            NotificationCenter.default.post(name: notificationName, object:nil , userInfo: nil)
+//            let notificationName = Notification.Name("BuyItem")
+//            NotificationCenter.default.post(name: notificationName, object:nil , userInfo: nil)
             
         }
         
@@ -93,6 +95,8 @@ class ShopCollectionViewController: UICollectionViewController {
         shopAlert.addAction(okAction)
         
         self.present(shopAlert, animated: true, completion: nil)
+        
+
         
         return shop.shopItem[sender.tag]
     }
