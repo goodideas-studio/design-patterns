992134d38d952da94e2f15e0c5fce7a1806e76e1
PinguMac <>
2018-02-27 23:43:53 +0800

    Add a class to present character information

--- Singleton/Ells_Gavin/Singleton_Game/Controllers/MainViewController.swift
+++ Singleton/Ells_Gavin/Singleton_Game/Controllers/MainViewController.swift
@@ -8,13 +8,28 @@
 
 import UIKit
 
+enum ItemAttribute: String {
+  case Attack = "Attack"
+  case Defence = "Defence"
+  case MP = "MP"
+}
+
 class MainViewController: UIViewController {
 
   @IBOutlet weak var totalAssetLabel: UILabel!
+  @IBOutlet weak var hpVal: UILabel!
+  @IBOutlet weak var atkVal: UILabel!
+  @IBOutlet weak var mpVal: UILabel!
+  @IBOutlet weak var defVal: UILabel!
+  
+  var character = Character(ATK: 100, DEF: 100, HP: 100, MP: 100)
   
   override func viewDidLoad() {
     super.viewDidLoad()
     // Do any additional setup after loading the view, typically from a nib.
+    
+    let packVC = tabBarController?.viewControllers![1] as! PackViewController
+    packVC.delegate = self
     
     // Initial all view controller in tab bar controller
     for viewController in (self.tabBarController?.viewControllers)! {
@@ -31,6 +46,18 @@ class MainViewController: UIViewController {
 
   @IBAction func giveMeMoney(_ sender: UIButton) {
     totalAssetLabel.text = String(Asset.shared.increaseMoney(Money: 5))
+  }
+}
+
+extension MainViewController: PackViewControllerDelegate {
+  func characterInfoUpdate(packItem: PowerUps) {
+    switch packItem.attribute {
+    case ItemAttribute.Attack.rawValue:
+      character.ATK += packItem.powerValue
+      atkVal.text = String(character.ATK)
+    default:
+      break
+    }
   }
 }
 
