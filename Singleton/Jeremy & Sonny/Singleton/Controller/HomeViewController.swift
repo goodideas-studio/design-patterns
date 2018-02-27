2c837af2f5de477c556821aa572a5e982a2f8ab2
Jeremy <gg831006@gmail.com>
2018-02-27 12:25:42 +0800

    刪去ContantView，直接在ShopViewController生成CollectionView

--- Singleton/Jeremy & Sonny/Singleton/Controller/HomeViewController.swift
+++ Singleton/Jeremy & Sonny/Singleton/Controller/HomeViewController.swift
@@ -14,7 +14,6 @@ class HomeViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        
         // Do any additional setup after loading the view, typically from a nib.
     }
 
--- Singleton/Jeremy & Sonny/Singleton/Controller/ShopViewController.swift
+++ Singleton/Jeremy & Sonny/Singleton/Controller/ShopViewController.swift
@@ -8,33 +8,95 @@
 
 import UIKit
 
+protocol ShopDelegate {
+    func PutInPack(itemName:String)
+}
 
 
-class ShopViewController: UIViewController {
+
+class ShopViewController: UIViewController , UICollectionViewDelegate, UICollectionViewDataSource {
     
+    // 創建 Shop 實例
+    var shop = Shop()
+    
+    // Delegate
+    var shopItemDelegate:ShopDelegate?
+    
+    
+    @IBOutlet weak var shopCollectionView: UICollectionView!
+    @IBOutlet weak var shopLayout: UICollectionViewFlowLayout!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
-
+        
+        // shop 排版
+        shopLayout.sectionInset = UIEdgeInsets(top: 20, left: 10, bottom: 10, right: 10)
+        shopLayout.itemSize = CGSize(width: 150 , height: 150)
+        shopLayout.minimumLineSpacing = CGFloat(integerLiteral: 10)
+        shopLayout.minimumInteritemSpacing = CGFloat(integerLiteral: 10)
+        
+        shopCollectionView.dataSource = self
+        shopCollectionView.delegate = self
+        
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+        
     }
     
-
-    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    // CollectionView協定
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return shop.shopItem.count
     }
-    */
-
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "cell", for: indexPath)
+        
+        let shopButton = UIButton(frame: CGRect(x: 0, y: 0, width: 150, height: 150))
+        shopButton.tag = indexPath.row
+        shopButton.setImage(UIImage(named: shop.shopItem[indexPath.row]), for: .normal)
+        shopButton.addTarget(self, action: #selector(showShopAlert), for: UIControlEvents.touchUpInside)
+        
+        cell.addSubview(shopButton)
+        
+        return cell
+        
+        
+    }
+    
+    @objc func showShopAlert(sender:UIButton) -> String{
+        
+        let shopAlert = UIAlertController(title: "Check", message: "確認要購買？", preferredStyle: UIAlertControllerStyle.alert)
+        
+        let cancelAction = UIAlertAction(title: "Cancel", style: UIAlertActionStyle.default) { (finish) in
+            print("取消購買")
+            
+        }
+        let okAction = UIAlertAction(title: "OK", style: UIAlertActionStyle.cancel) { (finish) in
+            print("確認購買")
+            
+            
+            // tabbar 方式傳值到 PackViewController
+            if let packVC = self.tabBarController?.viewControllers![1] as? PackViewController {
+                
+                // 增加 ItemInPack
+                packVC.ItemInPack.append(self.shop.shopItem[sender.tag])
+                // 修改標題 物品數量
+                packVC.itemCount.text = "ItemCount:\(packVC.ItemInPack.count)"
+                packVC.packColletionView.reloadData()
+            }
+            
+            self.shop.shopItem.remove(at: sender.tag)
+            self.shopCollectionView.reloadData()
+        }
+        shopAlert.addAction(cancelAction)
+        shopAlert.addAction(okAction)
+        
+        self.present(shopAlert, animated: true, completion: nil)
+        return shop.shopItem[sender.tag]
+        
+    }
+    
 }
