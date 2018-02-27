4a9a049ff649a4937b0b0e8a78c98d58f2bdd04d
PinguMac <>
2018-02-27 13:25:58 +0800

    Replace money notification to singleton

--- Singleton/Ells_Gavin/Singleton_Game/Controllers/PackViewController.swift
+++ Singleton/Ells_Gavin/Singleton_Game/Controllers/PackViewController.swift
@@ -20,21 +20,18 @@ class PackViewController: UIViewController {
   override func viewDidLoad() {
     super.viewDidLoad()
     // Do any additional setup after loading the view, typically from a nib.
+  
+  }
+  
+  override func viewDidAppear(_ animated: Bool) {
+    super.viewDidAppear(true)
     
-    NotificationCenter.default.addObserver(self,
-                                           selector: #selector(moneyNotify),
-                                           name: .moneyDidSelect,
-                                           object: nil)
+    totalAsset.text = String(Asset.shared.totalAsset)
   }
 
   override func didReceiveMemoryWarning() {
     super.didReceiveMemoryWarning()
     // Dispose of any resources that can be recreated.
-  }
-
-  @IBAction func moneyNotify(notification: NSNotification) {
-    receiveData = notification.object as! String
-    totalAsset.text = receiveData
   }
   
 }
