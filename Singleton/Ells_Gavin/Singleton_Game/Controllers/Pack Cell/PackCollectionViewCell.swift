ab6d3b1ac43a0549e7f963a53b9c92b2a64c7e8c
PinguMac <>
2018-02-27 16:26:14 +0800

    Item deleted after selected

---
+++ Singleton/Ells_Gavin/Singleton_Game/Controllers/Pack Cell/PackCollectionViewCell.swift
@@ -0,0 +1,19 @@
+//
+//  CellTypeOneCollectionViewCell.swift
+//  Singleton_Game
+//
+//  Created by 唐嘉伶 on 26/02/2018.
+//  Copyright © 2018 唐嘉伶. All rights reserved.
+//
+
+import UIKit
+
+class PackCollectionViewCell: UICollectionViewCell {
+    
+    @IBOutlet weak var itemInPackPic: UIImageView!
+    
+    @IBOutlet weak var itemInPackAffectType: UILabel!
+    
+    @IBOutlet weak var itemInPackPowerVal: UILabel!
+    
+}
--- Singleton/Ells_Gavin/Singleton_Game/Controllers/PackViewController.swift
+++ Singleton/Ells_Gavin/Singleton_Game/Controllers/PackViewController.swift
@@ -10,23 +10,26 @@ import UIKit
 
 class PackViewController: UIViewController {
     
-  var itemsInMyPack = [String]()
-  var receiveData: String!
+//  var itemsInMyPack = [String]()
     
     
   @IBOutlet weak var totalAsset: UILabel!
+  @IBOutlet weak var totalItems: UILabel!
   @IBOutlet weak var packItemsCollection: UICollectionView!
     
   override func viewDidLoad() {
     super.viewDidLoad()
     // Do any additional setup after loading the view, typically from a nib.
   
+    packItemsCollection.delegate = self
+    packItemsCollection.dataSource = self
   }
   
   override func viewDidAppear(_ animated: Bool) {
     super.viewDidAppear(true)
     
     totalAsset.text = String(Asset.shared.totalAsset)
+    totalItems.text = String(itemInPack.count)
   }
 
   override func didReceiveMemoryWarning() {
@@ -39,20 +42,24 @@ class PackViewController: UIViewController {
 
 // extension PackViewController to be delegate & datasrc of collectionView
 extension PackViewController: UICollectionViewDelegate {
-  
+  func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+    itemInPack.remove(at: indexPath.item)
+    self.packItemsCollection.reloadData()
+  }
 }
 
 extension PackViewController: UICollectionViewDataSource {
     
   func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
         
-    return itemsInMyPack.count
+    return itemInPack.count
   }
     
     
   func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-    let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "CellOne", for: indexPath) as! CellInMyBackPack
-        
+    let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "PackItemCell", for: indexPath) as! PackCollectionViewCell
+    cell.itemInPackPic.image = itemInPack[indexPath.item].potionImg
+    cell.itemInPackPowerVal.text = String(itemInPack[indexPath.item].powerValue)
     return cell
   }
     
