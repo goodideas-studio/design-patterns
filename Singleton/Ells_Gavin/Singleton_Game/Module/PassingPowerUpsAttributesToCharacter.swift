feb0b9b264a0a6cf705670742eb94edcc66fbbf3
ellstang <lilyc633@gmail.com>
2018-03-01 11:21:04 +0800

    1. Added README file

---
+++ Singleton/Ells_Gavin/Singleton_Game/Module/PassingPowerUpsAttributesToCharacter.swift
@@ -0,0 +1,14 @@
+//
+//  PassingPowerUpsAttributesToCharacter.swift
+//  Singleton_Game
+//
+//  Created by 唐嘉伶 on 01/03/2018.
+//  Copyright © 2018 唐嘉伶. All rights reserved.
+//
+
+import Foundation
+
+
+protocol PassingPowerUpsAttributesToCharacter {
+  func passPowerUpsToCharacter(item: PowerUps) 
+}
