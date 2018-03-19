3bd2a3a14ea5db0234f779bca1a090a633e07c68
PinguMac <>
2018-03-19 10:38:26 +0800

    Facade pattern demo initial

---
+++ Facade/Gavin/Gavin/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Gavin
+//
+//  Created by PinguMac on 2018/3/19.
+//  Copyright © 2018年 PinguMac. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+
+
+}
+
